a931a2ae4415642e17e08f21e8a24f1fbecdbe25
Rudan <rudanlucena@gmail.com>
2026-05-04 07:52:11 -0300

    chore: initial project setup (Angular + Spring Boot + PostgreSQL via Docker)

---
+++ backend/src/main/java/com/mauriti3d/backend/Mauriti3DApplication.java
@@ -0,0 +1,13 @@
+package com.mauriti3d.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mauriti3DApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mauriti3DApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mauriti3d/backend/Mauriti3DApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauriti3d.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mauriti3DApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
